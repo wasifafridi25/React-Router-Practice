6b8b0bc6b49eb2915b771ab1d19a08cb66fab22c
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-09-11 18:41:45 -0700

    Added the user Ronaldo

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home.jsx';
-import About from './pages/About';
-import Contact from './pages/Contact';
 import Nav from "./components/Nav"
+import Users from './pages/Users'
 
 function App() {
   return (
@@ -11,8 +10,9 @@ function App() {
         <Nav></Nav>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path='/contact' element={<Contact />} />
+          {/* <Route path="/about" element={<About />} />
+          <Route path='/contact' element={<Contact />} /> */}
+          <Route path="/users/:username" element={<Users /> } />
         </Routes>
       </Router>
   );
--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,3 +1,7 @@
 function Users(){
-    
+    return (
+        <h1>Users</h1>
+    );
 }
+
+export default Users;
