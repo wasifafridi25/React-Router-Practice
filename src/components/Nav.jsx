be6713388edbb513d7140b9378e40c9d0e5d65a4
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-09-11 18:33:50 -0700

    Added the users

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,8 +4,10 @@ function Nav(){
     return (
         <nav>
           <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/Contact">Contact</Link>
+          {/* <Link to="/about">About</Link>
+          <Link to="/Contact">Contact</Link> */}
+          <Link to="/users/Ronaldo">Ronaldo</Link>
+          <Link to="/users/Virat">Virat</Link>
         </nav>
     );
 }
