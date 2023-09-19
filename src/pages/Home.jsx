a7d456aad94b8aa1a986216a8fe9a909b90c479f
Bhuiyan Shadman Wasif <waseefafridi39@gmail.com>
2023-09-19 02:59:03 -0700

    Started axiosx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,16 @@
+import React, { useEffect } from "react"
+import axios from 'axios'
+
 function Home(){
+    async function fetchUsers(){
+        const { data } = await axios.get('https://jsonplaceholder.typicode.com/users')
+        console.log(data)
+    }
+    
+    useEffect(() => {
+        fetchUsers();
+    }, [])
+
     return <h1>Home</h1>
 }
 
